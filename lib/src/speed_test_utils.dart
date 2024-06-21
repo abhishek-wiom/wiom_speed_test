d9e9bbf1c104dff1cd6961438ef93255bfd2697c
Rahul Sharma <rahul.sharma@wiom.in>
2024-06-21 16:25:05 +0530

    WIP

--- lib/src/speed_test_utils.dart
+++ lib/src/speed_test_utils.dart
@@ -2,8 +2,11 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 
 Future<bool> isInternetAvailable() async {
   final connectivity = Connectivity();
-  final connectivityResult = await connectivity.checkConnectivity();
-  return connectivityResult == ConnectivityResult.mobile ||
-      connectivityResult == ConnectivityResult.wifi || 
-      connectivityResult == ConnectivityResult.ethernet;
+  final List<ConnectivityResult> connectivityResult =
+      await connectivity.checkConnectivity();
+  return (connectivityResult.indexWhere((element) =>
+          element == ConnectivityResult.mobile ||
+          element == ConnectivityResult.wifi ||
+          element == ConnectivityResult.ethernet) !=
+      -1);
 }
